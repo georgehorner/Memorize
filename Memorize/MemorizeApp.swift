f60f53b65406ded168f13e62d130cf83bf63b88e
George Horner <zcapgho@ucl.ac.uk>
2021-01-30 13:28:48 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by George Horner on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
